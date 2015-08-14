3a4bdda8545ef1bf64cf987e6f158cd2aac2f14b
oleg-codefresh <oleg@codefresh.io>
2015-08-14 13:44:10 +0000

    just for demo

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+//just for the demo
 ////
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
